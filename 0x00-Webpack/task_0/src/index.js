d290a0d0d9ca03f25613409503eb73f319bfc7a6
zackcis <zakaria.eldahar@gmail.com>
2024-07-22 12:15:03 +0100

    pushing

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // Add three different paragraphs to the body
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the student</p>');
+    $('body').append('<p>Copyrights - Holberton school</p>');
+});
