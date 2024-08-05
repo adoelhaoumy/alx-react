c79235b532e20913fbb7b173a242caaa514aa5e4
adoelhaoumy <alhoumi20@gmail.com.com>
2024-08-05 05:04:56 -0700

    react

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseShape.js
@@ -0,0 +1,8 @@
+mport PropTypes from 'prop-types';
+
+export default PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  credit: PropTypes.number.isRequired
+}
+);
